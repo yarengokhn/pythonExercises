baf91e0515baa4aa25b0375ac292251b4c726b93
yaren <yarengokhn@gmail.com>
2024-10-14 10:13:02 +0300

    binary search algorithm deleted from mongodb_test.py

--- mongodb_test.py
+++ mongodb_test.py
@@ -43,30 +43,5 @@ print(x.inserted_ids)
 
 
 
-def binary_search(array, target):
-  left = 0
-  right = len(array) - 1
-  while left <= right:
-    mid = (left + right) // 2
 
-    if array[mid] == target:
-      return mid
-      
-    if array[mid] < target:
-      left = mid + 1
-
-    else:
-      right = mid -1   
-
-  return -1
-
-arr1 = [1,3,5,7,9,11,13,15,17,19]
-targetValue = 15
-
-result = binary_search(arr1,targetValue)
-
-if result != -1:
-    print("Value",targetValue,"found at index", result)
-else:
-    print("Target not found in array.")
         
